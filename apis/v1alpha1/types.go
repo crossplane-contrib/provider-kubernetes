d838b1d3b14cc5ca4b6c7f92c6965d57c4b71327
Christoph Petrausch <christoph.petrausch@inovex.de>
2022-01-13 12:21:11 +0100

    Add canonical categories to ProviderConfig
    Signed-off-by: Christoph Petrausch <christoph.petrausch@inovex.de>

--- apis/v1alpha1/types.go
+++ apis/v1alpha1/types.go
@@ -71,7 +71,7 @@ type ProviderConfigStatus struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:printcolumn:name="SECRET-NAME",type="string",JSONPath=".spec.credentials.secretRef.name",priority=1
-// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:resource:scope=Cluster,categories={crossplane,provider,kubernetes}
 type ProviderConfig struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
