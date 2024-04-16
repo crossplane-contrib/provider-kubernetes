4ddcffed96048747be4ab18ff610e8b2305a2f79
Christian Artin <gravufo@gmail.com>
2024-04-16 13:47:35 -0400

    make reviewable
    Signed-off-by: Christian Artin <gravufo@gmail.com>

--- internal/clients/azure/azure_test.go
+++ internal/clients/azure/azure_test.go
@@ -20,8 +20,7 @@ func Test_kubeloginTokenOptionsFromRESTConfig(t *testing.T) {
 		}
 		if opts == nil {
 			t.Errorf("should not be nil")
-		}
-		if opts.ServerID != "" {
+		} else if opts.ServerID != "" {
 			t.Errorf("unexpected server id: %q", opts.ServerID)
 		}
 	})
@@ -39,8 +38,7 @@ func Test_kubeloginTokenOptionsFromRESTConfig(t *testing.T) {
 		}
 		if opts == nil {
 			t.Errorf("should not be nil")
-		}
-		if opts.ServerID != serverID {
+		} else if opts.ServerID != serverID {
 			t.Errorf("unexpected server id: %q, expected: %q", opts.ServerID, serverID)
 		}
 	})
