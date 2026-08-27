3716333f17af5c42ee618da3c7e45827273a9e1a
Jonasz Łasut-Balcerzak <jonasz@upbound.io>
2026-08-27 12:34:12 +0200

    Fix ObservedObjectCollection failing to create Objects due to incorrectly serialized deletionPropagationPolicy
    Signed-off-by: Jonasz Łasut-Balcerzak <jonasz@upbound.io>

--- apis/namespaced/object/v1alpha1/types.go
+++ apis/namespaced/object/v1alpha1/types.go
@@ -85,7 +85,7 @@ type ObjectParameters struct {
 	// +optional
 	// +kubebuilder:validation:Enum=Orphan;Background;Foreground
 	// +kubebuilder:default=Background
-	DeletionPropagationPolicy metav1.DeletionPropagation `json:"deletionPropagationPolicy"`
+	DeletionPropagationPolicy metav1.DeletionPropagation `json:"deletionPropagationPolicy,omitempty"`
 }
 
 // ObjectObservation are the observable fields of a Object.
